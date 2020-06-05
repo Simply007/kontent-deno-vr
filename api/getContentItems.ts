85482d7e0e9184ad888e2647fb2de9cc84ba18a0
ondrejch <ondrejch@kentico.com>
2020-06-05 13:36:16 +0200

    test5

--- api/getContentItems.ts
+++ api/getContentItems.ts
@@ -4,7 +4,7 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 const getContentItems = async (querystring: string = "") => {
   const pid = config().PROJECT_ID;
   const url = `https://deliver.kontent.ai/${pid}/items?${querystring}`;
-
+  console.log(`Getting data from ${url}`);
   return await ky.get(url).json();
 };
 
