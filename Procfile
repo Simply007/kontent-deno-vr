5067766fa3c3d487785467e54e88857a08edb2fe
ondrejch <ondrejch@kentico.com>
2020-06-05 13:03:32 +0200

    alosaurus

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net --allow-write --allow-read --allow-env --config tsconfig.json alosaur.ts --port=${PORT}
+web: deno run --unstable --allow-net --allow-write --allow-read --allow-env --config tsconfig.json alosaur.ts --port=${PORT}
--- alosaur.ts
+++ alosaur.ts
@@ -6,7 +6,6 @@ import { ListingArea } from "./areas/listing/listing.area.ts";
 import { App, ViewRenderConfig } from "https://deno.land/x/alosaur/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
-import { serve } from "https://deno.land/std@0.55.0/http/server.ts";
 
 const { args, exit } = Deno;
 const DEFAULT_PORT = 8080;
@@ -18,28 +17,22 @@ if (isNaN(port)) {
   exit(1);
 }
 
-// const appSettings = {
-//   areas: [HomeArea, ListingArea, TourArea],
-//   logging: true,
-// };
+const appSettings = {
+  areas: [HomeArea, ListingArea, TourArea],
+  logging: true,
+};
 
-// const app = new App(appSettings);
-// app.useStatic({
-//   root: `${Deno.cwd()}/assets`,
-//   baseRoute: "/assets/",
-// });
-// app.useViewRender({
-//   type: "dejs",
-//   basePath: `${Deno.cwd()}/views/`,
-//   getBody: (path: string, model: Object, config: ViewRenderConfig) =>
-//     renderFile(normalize(`${config.basePath}${path}.ejs`), model),
-// });
+const app = new App(appSettings);
+app.useStatic({
+  root: `${Deno.cwd()}/assets`,
+  baseRoute: "/assets/",
+});
+app.useViewRender({
+  type: "dejs",
+  basePath: `${Deno.cwd()}/views/`,
+  getBody: (path: string, model: Object, config: ViewRenderConfig) =>
+    renderFile(normalize(`${config.basePath}${path}.ejs`), model),
+});
 
-// console.log(`Start listening on port: ${port}`);
-// app.listen(`:${port}`);
-
-const s = serve({ port });
-console.log(`http://localhost:${port}/`);
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+console.log(`Start listening on port: ${port}`);
+app.listen(`:${port}`);
